ae29c02a763b379841bc85c8433ef729eac02c0c
LAPTOP-OOGIJMVI\סיון <guy0204@gmail.com>
2023-09-05 16:10:21 +0300

    change web env1

--- shell_scripts/web_script.sh
+++ shell_scripts/web_script.sh
@@ -7,7 +7,7 @@ apt install python3
 apt install python3-pip -y
 pip install flask
 pip install psycopg2-binary
-source /var/lib/waagent/custom-script/download/0/terraform_project/shell_scripts/export_script.bash "$1" "$2" "$3" "$4"
+source /var/lib/waagent/custom-script/download/0/terraform_project/shell_scripts/ "$1" "$2" "$3" "$4"
 
 # Clone repo and run scripts
 git clone https://github.com/Guyashkenazi6/terraform_project.git
